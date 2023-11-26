0598cf12b6cbe6448920ff4ac47c575cee290bbe
kitaee <gtlee@adcapsule.co.kr>
2023-11-26 21:24:54 +0900

    [FEAT]: HTMLExporter 구현

--- src/main/java/com/example/demo/holub/database/HTMLExporter.java
+++ src/main/java/com/example/demo/holub/database/HTMLExporter.java
@@ -1,42 +1,51 @@
 package com.example.demo.holub.database;
 
+import lombok.RequiredArgsConstructor;
+
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Iterator;
 
+/**
+ * 데이터베이스 테이블 정보를 HTML로 Export 해주는 class
+ * */
+
+@RequiredArgsConstructor
 public class HTMLExporter implements Table.Exporter {
-    private final Writer out;
 
-    public HTMLExporter( Writer out ) {
-        this.out = out;
-    }
+    private final Writer writer;
+    private final IteratorConverter iteratorConverter;
+
     public void startTable() throws IOException {
-        out.write("<html>\n");
-        out.write("<body>\n");
+        writer.write("<html>\n");
+        writer.write("<body>\n");
     }
-    public void storeMetadata( String tableName, int width, int height, Iterator columnNames ) throws IOException {
-        tableName = tableName == null ? "anonymous" : tableName;
-        out.write(String.format("\t<h1>%s</h1>\n", tableName));
-        out.write("\t<table>\n");
-        out.write("\t\t<tr>\n");
-        while(columnNames.hasNext()){
-            Object columnName = columnNames.next();
-            out.write(String.format("\t\t\t<th>%s</th>\n", columnName));
+    public void storeMetadata(String tableName, int width, int height, Iterator columnNames) throws IOException {
+        writer.write("\t<table>\n");
+        writer.write("\t\t<tr>\n");
+
+        Iterable<String> iterableColumnNames = iteratorConverter.convertToIterable(columnNames);
+
+        for (String columnName : iterableColumnNames) {
+            writer.write(String.format("\t\t\t<th>%s</th>\n", columnName));
         }
-        out.write("\t\t</tr>\n");
+        writer.write("\t\t</tr>\n");
     }
 
-    public void storeRow( Iterator data ) throws IOException {
-        out.write("\t\t<tr>\n");
-        while( data.hasNext() ) {
-            Object datum = data.next();
-            out.write(String.format("\t\t\t<td>%s</td>\n", datum));
+    public void storeRow(Iterator data) throws IOException {
+        writer.write("\t\t<tr>\n");
+
+        Iterable<String> iterableDatas = iteratorConverter.convertToIterable(data);
+
+        for(String iterableData : iterableDatas) {
+            writer.write(String.format("\t\t\t<td>%s</td>\n", iterableData));
         }
-        out.write("\t\t</tr>\n");
+
+        writer.write("\t\t</tr>\n");
     }
-    public void endTable()   throws IOException {
-        out.write("\t</table>\n");
-        out.write("</body>\n");
-        out.write("</html>");
+    public void endTable() throws IOException {
+        writer.write("\t</table>\n");
+        writer.write("</body>\n");
+        writer.write("</html>");
     }
 }
