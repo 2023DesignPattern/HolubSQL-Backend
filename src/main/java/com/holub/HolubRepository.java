74d09c9bde0e39935d68947d2e51f79a634a1f1d
kitaee <gtlee@adcapsule.co.kr>
2023-11-26 23:23:29 +0900

    [FEAT]: HolubRepository 구현

--- src/main/java/com/holub/HolubRepository.java
+++ src/main/java/com/holub/HolubRepository.java
@@ -1,4 +1,29 @@
 package com.holub;
 
+import com.holub.database.Database;
+import com.holub.database.Table;
+import com.holub.text.ParseFailure;
+
+import java.io.File;
+import java.io.IOException;
+
 public class HolubRepository {
+
+    Database database;
+
+    public HolubRepository() {
+        try {
+            database = new Database(new File("com/holub/database/Dbase"));
+        } catch (IOException e) {
+            throw new RuntimeException("DB ERROR");
+        }
+    }
+
+    public Table getTable(String query){
+        try {
+            return database.execute(query);
+        } catch (IOException | ParseFailure e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
